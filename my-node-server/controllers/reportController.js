f7c6706c1e6b841488a72fd5e07ac99511321ab6
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-30 12:49:04 +0700

    praktikum 5

--- my-node-server/controllers/reportController.js
+++ my-node-server/controllers/reportController.js
@@ -7,3 +7,28 @@ exports.getDailyReport = (req, res) => {
     data: presensiRecords,
   });
 };
+
+const { Presensi } = require("../models");
+const { Op } = require("sequelize");
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    const { nama } = req.query;
+    let options = { where: {} };
+
+    if (nama) {
+      options.where.nama = {
+        [Op.like]: `%${nama}%`,
+      };
+    }
+
+    const records = await Presensi.findAll(options);
+
+    res.json({
+      reportDate: new Date().toLocaleDateString(),
+      data: records,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Gagal mengambil laporan", error: error.message });
+  }
+};
